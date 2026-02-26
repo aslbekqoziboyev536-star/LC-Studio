9abeb9b8315f39ad47c9395773075b66f741804a
aslbekqoziboyev536-star <aslbekqoziboyev536@gmail.com>
2026-02-26 08:55:49 +0500

    Update config.ts

--- config.ts
+++ config.ts
@@ -2,4 +2,4 @@
 // Agar lokalda ishlatayotgan bo'lsangiz: 'http://localhost:5000/api'
 // Agar deploy qilingan bo'lsa (Render/Railway): 'https://sizning-backend-url.com/api'
 
-export const API_BASE_URL = '/api'; 
+export const API_BASE_URL = 'https://lc-template-backend.onrender.com/api'; 
